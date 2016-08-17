5005f13d592159faf451f86645779be4beae02de
Caitlin Hayles <cjhayles93@gmail.com>
2016-08-17 11:55:10 +0200

    Code for signing in with routing problems

--- GetSmarterOrders.js
+++ GetSmarterOrders.js
@@ -3,12 +3,35 @@ var app = angular.module('OrdersApp', ['ngRoute', 'firebase']);
 app.config(function($routeProvider){
   $routeProvider.when('/', {
     controller: 'OrdersListCtrl',
-    templateUrl: 'templates/orders_list.html'
+    templateUrl: 'templates/orders_list.html',
+    // resolve: {
+    //       "currentAuth": function($firebaseAuth) {
+    //           return $firebaseAuth().$requireSignIn();
+    //       }
+    //   }
   }).when('/:orderID', {
     controller: 'CurrentOrderCtrl',
-    templateUrl: 'templates/current_order.html'
+    templateUrl: 'templates/current_order.html',
+    // resolve: {
+    //       "currentAuth": function($firebaseAuth) {
+    //           return $firebaseAuth().$requireSignIn();
+    //       }
+    //   }
+  }).when('/signin', {
+    controller: 'SignInCtrl',
+    templateUrl: 'templates/sign_in.html'
   })
 })
+
+// app.run(["$rootScope", "$location", function($rootScope, $location) {
+//   $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+//     // We can catch the error thrown when the $requireSignIn promise is rejected
+//     // and redirect the user back to the home page
+//     if (error === "AUTH_REQUIRED") {
+//       $location.path("/signin");
+//     }
+//   });
+// }]);
 
 app.controller('CurrentOrderCtrl', function($scope, $firebaseArray, $routeParams){
   $scope.isHome = true;
@@ -53,3 +76,51 @@ app.controller('OrdersListCtrl', function($scope, $firebaseArray){
   }
 
 })
+
+app.controller('SignInCtrl', function($scope, $firebaseAuth){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  console.log("WHY?!")
+
+  $scope.userLogin = function(){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+    // ...
+    });
+  }
+
+    //  function onSignIn(googleUser) {
+    //   // Useful data for your client-side scripts:
+    //   var profile = googleUser.getBasicProfile();
+    //   console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    //   console.log('Full Name: ' + profile.getName());
+    //   console.log('Given Name: ' + profile.getGivenName());
+    //   console.log('Family Name: ' + profile.getFamilyName());
+    //   console.log("Image URL: " + profile.getImageUrl());
+    //   console.log("Email: " + profile.getEmail());
+
+    //   // The ID token you need to pass to your backend:
+    //   var id_token = googleUser.getAuthResponse().id_token;
+    //   console.log("ID Token: " + id_token);
+    // };
+})
+
+
+
+
+
+
+
+
+
