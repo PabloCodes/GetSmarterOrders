3563c0ca1f9fce5eed536fce4e829497931b269b
Caitlin Hayles <cjhayles93@gmail.com>
2016-08-17 10:57:19 +0200

    Functioning Ordering Page

--- GetSmarterOrders.js
+++ GetSmarterOrders.js
@@ -4,25 +4,39 @@ app.config(function($routeProvider){
   $routeProvider.when('/', {
     controller: 'OrdersListCtrl',
     templateUrl: 'templates/orders_list.html'
-  }).when('/CurrentOrder', {
+  }).when('/:orderID', {
     controller: 'CurrentOrderCtrl',
     templateUrl: 'templates/current_order.html'
   })
 })
 
-app.controller('CurrentOrderCtrl', function($scope, $firebaseArray){
-  isHome = true;
-  atAccount = false;
-  isSignedIn = true;
-  mustSignIn = false;
+app.controller('CurrentOrderCtrl', function($scope, $firebaseArray, $routeParams){
+  $scope.isHome = true;
+  $scope.atAccount = false;
+  $scope.isSignedIn = true;
+  $scope.mustSignIn = false;
+
+  var ref = firebase.database().ref().child("orders");
+  $scope.orders = $firebaseArray(ref);
+
+  var ref = firebase.database().ref().child("orderItems").child($routeParams.orderID);
+  $scope.orderItems = $firebaseArray(ref);
+  $scope.addOrderItem = function(){
+    $scope.orderItems.$add({
+      item: $scope.item,
+      cost: $scope.cost,
+      created_by: $scope.name
+    });
+
+  }
 
 })
 
 app.controller('OrdersListCtrl', function($scope, $firebaseArray){
-  isHome = true;
-  atAccount = false;
-  isSignedIn = true;
-  mustSignIn = false;
+  $scope.isHome = true;
+  $scope.atAccount = false;
+  $scope.isSignedIn = true;
+  $scope.mustSignIn = false;
 
   var ref = firebase.database().ref().child("orders");
   $scope.orders = $firebaseArray(ref);
